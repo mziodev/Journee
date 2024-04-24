b8b3cd92af84609bfd13be5faceff53f670914f0
MZIO <mziodev@gmail.com>
2024-04-24 13:33:40 +0200

    Initial Commit

---
+++ Journee/JourneeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneeApp.swift
+//  Journee
+//
+//  Created by Mauricio dSR on 24/4/24.
+//
+
+import SwiftUI
+
+@main
+struct JourneeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
