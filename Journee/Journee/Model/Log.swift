39e18a7a65cad75f1c672b21f150279833a2ecd0
MZIO <mziodev@gmail.com>
2024-05-07 15:56:34 +0200

    [feature] Update journee fake data with one more Log. Some code styling.

--- Journee/Journee/Model/Log.swift
+++ Journee/Journee/Model/Log.swift
@@ -14,7 +14,14 @@ struct Log {
 }
 
 let journee = [
-    Log(creationDate: .now, modificationDate: .now, content: "This is my first entry for the Journee app, what do you think?"),
-    Log(creationDate: .now + 86400, modificationDate: .now + 86400, content: "This is my second entry for my journee."),
-    Log(creationDate: .now + (86400 * 3), modificationDate: .now + (86400 * 3), content: "This is my third entry for Journee.")
+    Log(creationDate: .now - (86400 * 3),
+        modificationDate: .now - (86400 * 3),
+        content: "This is my first entry for the Journee app, what do you think?"),
+    Log(creationDate: .now - (86400 * 2),
+        modificationDate: .now - (86400 * 2),
+        content: "This is my second entry for my journee."),
+    Log(creationDate: .now - 86400,
+        modificationDate: .now - 86400, content: "This is my third entry for Journee."),
+    Log(creationDate: .now,
+        modificationDate: .now, content: "This is my fourth entry for the Journee app.")
 ]
